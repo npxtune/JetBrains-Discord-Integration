de36b6ca52bbe021a894aac249267b1bab118800
Axel JOLY <azn9.pro@gmail.com>
2023-10-14 15:33:27 -0400

    feat: update copyright header

--- uploader/build.gradle.kts
+++ uploader/build.gradle.kts
@@ -1,11 +1,12 @@
 /*
- * Copyright 2017.2020 Aljoscha Grebe
+ * Copyright 2017-2020 Aljoscha Grebe
+ * Copyright 2017-2020 Axel JOLY (Azn9) - https://github.com/Azn9
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *   https://www.apache.org/licenses/LICENSE.2.0
+ *   https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
